4314278d0362464c8fdc424d43a6d3c538e9e036
Dileep <dileepvraj50@gmail.com>
2024-07-24 09:16:37 +0530

    sixsth file

---
+++ hiding_user_input_from_terminal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+read USERNAME
+
+echo "username is $USERNAME"
+
+echo "please enter your password:"
+
+read -s PASSWORD
+
+echo "password is $PASSWORD"
+
