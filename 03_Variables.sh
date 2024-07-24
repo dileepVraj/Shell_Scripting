0510d7c5e6bbaf8662462a4e56bf48521c0c7c0a
Dileep <dileepvraj50@gmail.com>
2024-07-24 08:53:26 +0530

    third file

---
+++ 03_Variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+USER1=Dileep
+USER2=Raja
+
+echo "Hey $USER1 do you know that continuous integration, delivery and deployment are key aspects in DevOps engineering."
+echo "Yes $USER2 I know that ;)"
