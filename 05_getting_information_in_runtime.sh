2e413d975e46feb0a982b17ac4ee56927274d6b3
Dileep <dileepvraj50@gmail.com>
2024-07-24 09:12:16 +0530

    fifth file

---
+++ 05_getting_information_in_runtime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your first name"
+
+read FIRST_NAME
+
+echo "please enter last name"
+
+read LAST_NAME
+
+echo first name is $FIRST_NAME and last name is $LAST_NAME
+
+
