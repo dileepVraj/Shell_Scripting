65b9200937c3dbd907d3895e5a4ce23f36a3414a
Dileep <dileepvraj50@gmail.com>
2024-07-24 09:08:08 +0530

    fourth file

---
+++ 04_using_arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# in this script we predefine some placeholders or variables in echo statements and we use arguments to pass values
+# while executing the script file.
+
+FIRST_NAME=$1
+LAST_NAME=$2
+
+echo "First name is $FIRST_NAME ."
+echo "Last name is $LAST_NAME. "
+
