c19f3ced617b05cd2deff0f6c172696590e8bbd9
Dileep <dileepvraj50@gmail.com>
2024-07-24 08:41:58 +0530

    second file

---
+++ 02_normal_conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hey Dileep do you know this Continuous integration Continuous delivery & Continuous deployement are important aspects of Devops"
+echo "Yes Raja I do know that ;)"
+
