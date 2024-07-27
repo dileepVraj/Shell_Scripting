962aff830899105bf9c92ba3cced6eb745d0182e
dileep50 <dileepvraj50@gmail.com>
2024-07-27 11:30:48 +0530

    mysql file 3rd change

--- MySQL_Installation.sh
+++ MySQL_Installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ID=$(id -u)
-if[ $ID -ne 0 ]
-then
-echo "Can't install services without sudo privileges"
+if[ $ID -ne 0 ];then
+
+    echo "Can't install services without sudo privileges"
 fi
 
