0667201b0ee8879fd438704f5ba6464cbbb86678
dileep50 <dileepvraj50@gmail.com>
2024-07-28 12:01:42 +0530

    MySQL_Installation_Version2.sh

--- MySQL_Installation_Version2.sh
+++ MySQL_Installation_Version2.sh
@@ -6,4 +6,12 @@ Username=$(whoami)
 if [ $ID -ne 0 ]
 then
     echo "looks you are not a root user. The actual user name is: $Username"
+    echo " Do you want to install MysQl client with user: $Username ?"
+    echo " Please enter your response 'y' if yes 'n' if no "
+    read RESPONSE
+    if [ $RESPONSE == "y" ]
+    then
+        sudo apt-get install mysql-server -y
+    fi
+
 fi
