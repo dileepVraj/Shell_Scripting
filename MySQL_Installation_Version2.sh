25adcac01ba7160c1356771e05c974346d0c7328
dileep50 <dileepvraj50@gmail.com>
2024-07-28 11:50:30 +0530

    modified Username variable

--- MySQL_Installation_Version2.sh
+++ MySQL_Installation_Version2.sh
@@ -2,7 +2,8 @@
 
 # fetch and store id of the user in a variable.
 ID=$(id -u)
+Username=$(whoami)
 if [ $ID -ne 0 ]
 then
-    "echo looks you are not a root user the user name is : $Username=$(whoami)"
+    "echo looks you are not a root user the actual user name is : $Username"
 fi
