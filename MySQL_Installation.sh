a891019ab8be17d669a17ef3a64c272fc3058cc7
dileep50 <dileepvraj50@gmail.com>
2024-07-29 17:16:05 +0530

    renamed 'first script.sh to 01_first script.sh' and modified login of MySQL_installation.sh

--- MySQL_Installation.sh
+++ MySQL_Installation.sh
@@ -2,18 +2,24 @@
 
 ID=$(id -u)
 # Executing 'id -u' command and storing value in variable ID.
+# 'id -u' command returns user id in numbers, where if the user is root user the value will be 0,
+# if the user is different from root user then the value will be anything from 0.
+
 
 if [ $ID -ne 0 ]
-# This if condition checks ID is not equal to value 0.
+# This if condition checks ID is not equal to value 0(if not root user).
 # if true then it executes the code under then block.
 then
 # If ID is not equal to 0 then prints the below message to the console.
     echo "Can't install services without sudo privileges"
 # This exit 1 is very helpful to stop the program execution fail at any certain point.
     exit 1
+elif [ $ID -eq 0 ]
+
+    # trying to install mysql client...
+    dnf install mysql -y
 fi
-# trying to install mysql client...
-dnf install mysql -y
+
 
 # Checking is mysql installation is successfull
 
@@ -22,6 +28,6 @@ then
     echo "MySql installation failed...."
     exit 1
 else
-    echo "MySql is already installed..."
+    echo "MySql successfully installed..."
 fi
 
