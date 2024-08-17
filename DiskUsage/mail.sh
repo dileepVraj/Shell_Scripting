e5f8852c0f9051e49fa2f53ef0ceabed4722239b
Dileep <dileepvraj50@gmail.com>
2024-08-17 16:04:28 +0530

    common message

--- DiskUsage/mail.sh
+++ DiskUsage/mail.sh
@@ -6,7 +6,7 @@ disk_usage_records=$(df -hT | grep -vE 'tmp|File|overlay')
 while IFS= read -r line
 do
 disk_usage="$(echo $line | awk '{ print $6 }' | cut -d % -f 1)"
-    if [ $disk_usage -ge $threshold ]; then
+    if [ "$disk_usage" -ge "$threshold" ]; then
     echo $line
     fi
 done <<< $disk_usage
