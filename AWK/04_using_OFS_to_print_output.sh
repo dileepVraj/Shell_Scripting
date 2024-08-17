01cf0183054e7c07f923cba0929148c8353aaa08
Dileep <dileepvraj50@gmail.com>
2024-08-17 12:48:45 +0530

    common message

--- AWK/04_using_OFS_to_print_output.sh
+++ AWK/04_using_OFS_to_print_output.sh
@@ -6,12 +6,25 @@
 echo "enter a sentence"
 read sentence
 
+# Explaination: printing number of fields count using substitution.
 echo "Number of fields in user specified sentence are:" $(echo $sentence | awk '{print NF}')
 
 
 echo "Printing them with Output Field Seperator"
 
+
 echo $sentence | awk 'BEGIN { OFS=":" } { print $1,$2,$3 }'
+
+Explaination:
+    # 'echo $sentence ' piping value of 'sentence' variable to to 'awk'.
+    # awk (text processor)
+    # 'BEGIN' is a special block that allows us to execute actions before any input lines are processed.
+    # This block is typically used to set variables, initialize counters, or perform any setup 
+    # operations that should occur before AWK starts reading the input data.
+    # {OFS=':'} Specifying the delimiter that output should printed.
+    
+
+
 
 
 
--- DiskUsage/disk_monitor.sh
+++ DiskUsage/disk_monitor.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-disk_usage=$(df -hT | grep -vE 'tmp|File')
-disk_threshold=$1
-message=""
+# problem?
+    # Print a warning message with file system when disk usage threshold is equal to or greater than 1%.
 
-while IFS= read line
+# Algorithm:
+    # 1. output the disk usage of machine.
+    # 2. filter out records except the file system.
+    # 3. store the file_system type value and disk usage value in separate variables.
+    # 4. verify the threashold or usage.
+    # 5. print warning message if usage is equal or greater than 1 if not print just usage.
+
+DISK_USAGE=$(df -hT)
+threshold=""
+fileSystemType=""
+
+while IFS= read -r line
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    echo $line | awk '{ print $1F }
+done <<< $DISK_USAGE
 
-done <<< $disk_usage
+
 
 
