f5a6b1a8e433bfdfa48d86fa304c63b080c370fd
Dileep <dileepvraj50@gmail.com>
2024-08-20 21:36:55 +0530

    message common for

--- DiskUsage/mail.sh
+++ DiskUsage/mail.sh
@@ -9,6 +9,7 @@ while IFS= read -r line
 do
     disk_usage_percentage=$(echo $line | awk '{print $6}' | cut -d % -f 1)
     if [ $disk_usage_percentage -ge $threshold ]; then
+        echo "Sending email: $disk_usage_percentage"
         echo "Warning disk usage reached to maxium threshold range: $disk_usage_percentage" | mail -s "Disk Usage" ananthvraj50@gmail.com
     fi
 done <<< $disk_usage
