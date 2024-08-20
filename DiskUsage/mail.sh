ba68641a462cabc73091e50070e72bcf8b8d33cb
Dileep <dileepvraj50@gmail.com>
2024-08-20 21:01:01 +0530

    message common for

--- DiskUsage/mail.sh
+++ DiskUsage/mail.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 threshold=1
+disk_usage=$(df -hT | grep -vE 'tmp|File|overlay')
 
 
 while IFS read -r line
 
 do
     echo $line
-done <<< df -hT | grep -vE 'tmp|File|overlay'  
+done <<< $disk_usage
 
 
