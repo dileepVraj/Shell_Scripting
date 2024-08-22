a2ca3e7f882097e90618b48047356349c209929d
Dileep <dileepvraj50@gmail.com>
2024-08-22 22:00:24 +0530

    message common for

--- z_ScratchDirectory/getOpts.sh
+++ z_ScratchDirectory/getOpts.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+usage(){
+    echo "Usage function called"
+}
+
 dish=""
 spicy=""
-sause=""
-
-usage(){
-    echo " usage function called "
-}
+sauce=""
 
 while getopts ":d:s:t:h" opt; do
     case $opt in
         d) dish="$OPTARG";;
         s) spicy="$OPTARG";;
-        t) sause="$OPTARG";;
-        h) echo "HelP"; exit 0;;
+        t) sauce="$OPTARG";;
+        h) echo "Help"; exit 0;;
         :) echo "Please specify options with arguments"; usage; exit 1;;
         \?) echo "Error: Invalid option: -$OPTARG"; exit 1;;
     esac
 done
 
-echo "here is your dish $dish which is $spicy spicy with $sause sause"
+echo "Here is your dish $dish which is $spicy spicy with $sauce sauce"
