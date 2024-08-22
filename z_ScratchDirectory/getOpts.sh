63f3bbcbffc1d02afb08ba7d971eca211a5ca280
Dileep <dileepvraj50@gmail.com>
2024-08-22 21:57:34 +0530

    message common for

--- z_ScratchDirectory/getOpts.sh
+++ z_ScratchDirectory/getOpts.sh
@@ -13,12 +13,9 @@ while getopts ":d:s:t:h" opt; do
     d) dish="$OPTARG";;
     s) spicy="$OPTARG";;
     t) sause="$OPTARG";;
-    h) echo "HelP" exit 0;;
+    h) echo "HelP"; exit 0;;
     :) echo "Please specify options with arguments"; usage; exit 1;;
-    /?) echo "Error: Invalid option: -$OPTARG"; exit 1;;
-    
-
-
+    \?) echo "Error: Invalid option: -$OPTARG"; exit 1;;
     esac
 done
 
