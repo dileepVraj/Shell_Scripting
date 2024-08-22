179125eab33b0685345665ef04c272d4020c62a2
Dileep <dileepvraj50@gmail.com>
2024-08-22 20:07:10 +0530

    message common for

---
+++ z_ScratchDirectory/getOpts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dish=""
+spicy=""
+sause=""
+
+while getopts=":d:s:t:h" opt; do
+case $opt in
+d) $dish="$OPTARG";;
+s) $spicy="$OPTARG";;
+t) $sause="$OPTARG";;
+esac
+done
+
+echo "here is your dish $dish which is $spicy spicy with $sause sause"
