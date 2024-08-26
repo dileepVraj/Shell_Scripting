feb1f0bb89509935c797a0d89511cbea0533c520
Dileep <dileepvraj50@gmail.com>
2024-08-26 16:10:47 +0530

    common message

--- 11_IAM/CreatingInstances.sh
+++ 11_IAM/CreatingInstances.sh
@@ -6,6 +6,7 @@ Security_Group_ID="sg-0c6b243516a699b3f" # Specify the security group ID to asso
 # Array of instance names for various services.
 # Instances_Array=("Mongo_db" "Catalogue" "Redis" "My_SQL" "Rabbit_MQ" "User" "Cart" "Shipping" "Payment" "Dispatch" "Web")
 Instances_Array=("Mongo_db" "Web" "Cart")
+Domain_Name="devopsraja4523.online"
 # Loop through each service name in the Instances_Array
 for i in "${Instances_Array[@]}"
 do
@@ -34,45 +35,27 @@ do
 
     #=====================================================================================
 
-
-    # Run the AWS CLI command to change resource record sets in Route 53
-aws route53 change-resource-record-sets \
-
-# Specify the ID of the hosted zone where the DNS record will be created
---hosted-zone-id $Zone_ID \
-
-# Define the changes to be made in a JSON format
---change-batch '
-{
-    # An optional comment to describe the purpose of the change batch
-    "Comment": "Creating a record for cognito endpoint",
-
-    # List of changes to apply
-    "Changes": 
-    [{
-        # The action to be performed. In this case, we're creating a new record
-        "Action": "CREATE",
-
-        # Details of the DNS record to be created
-        "ResourceRecordSet": 
-        {
-            # The fully qualified domain name (FQDN) for the record
-            # $i and $Domain_Name are shell variables
-            "Name": "'$i'.'$Domain_Name'",
-
-            # The type of DNS record (e.g., 'A' for IPv4 address)
-            "Type": "A",
-
-            # Time to Live (TTL) in seconds. The TTL determines how long DNS resolvers should cache the record
-            "TTL": 1,
-
-            # The value of the record. For 'A' records, this is the IP address
-            # $IP_ADDRESS is a shell variable containing the IP address
-            "ResourceRecords": 
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $Hosted_Zone_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for congnito endpoint"
+        ,"Changes": 
+        [{
+            "Action"    : "CREATE"
+            ,"ResourceRecordSet"    : 
+            {
+            "Name"  : "'$i'.'$Domain_Name'"
+            ,"Type" : "A"
+            ,"TTL"  : 1
+            ,"ResourceRecords"  : 
             [{
-                "Value": "'$IP_ADDRESS'"
+                "Value" : "'$Ip_Address'"
             }]
-        }
-    }]
-}'
+
+            }
+        }]
+    }
+        '
+    
 done
